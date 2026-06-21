f4a336c8f63c1645e6d0ddbcfda7724e29428e86
JeepWay <william.liu0731@gmail.com>
2026-06-21 15:53:58 +0800

    feat: update state transition flow

--- Core/Src/main.c
+++ Core/Src/main.c
@@ -87,7 +87,21 @@ int main(void)
   static uint32_t dist = 0;
 
   static lid_state_t lid_state = LID_CLOSED;
-  static uint32_t lid_timer_tick = 0;
+  static uint32_t lid_open_tick = 0;
+
+  // ===== Contextual adaptive variable =====
+  static uint16_t current_hold_time = LID_HOLD_TIME; 
+  static uint16_t saved_hold_time = LID_HOLD_TIME;   // save in eeprom
+  static uint16_t normal_use_count = 0;
+  static uint32_t last_close_tick = 0;              
+  static bool need_cool_down_check = false;          // turn TRUE after lid fully closed
+  #define EEPROM_HOLD_TIME_ADDR   0x0000
+  #define COOL_DOWN_TIMEOUT       15000   // timeout for check nobody is using
+  #define TIME_INCREMENT          1000    // for current_hold_time
+  #define MAX_HOLD_TIME           20000
+  #define MIN_HOLD_TIME           1000
+  #define DECREASE_THRESHOLD      3
+  // ========================================
   /* USER CODE END 1 */
 
   /* MCU Configuration--------------------------------------------------------*/
@@ -116,12 +130,36 @@ int main(void)
   /* Initialize interrupts */
   MX_NVIC_Init();
   /* USER CODE BEGIN 2 */
+  // ===== init device =====
   DWT_Delay_Init();
   LOG_INFO("System Clock: %lu\n", HAL_RCC_GetHCLKFreq());
   hcsr04_init_dev(&hcsr04_1, &htim3, TIM_CHANNEL_1, 
     HAL_TIM_ACTIVE_CHANNEL_1, Hcsr04_Trig_GPIO_Port, Hcsr04_Trig_Pin);
   sg90_init_dev(&sg90, &htim1, TIM_CHANNEL_4);
   eeprom_init_dev(&eeprom_08, &hi2c1, EEP_MODEL_AT24C08, 0);
+  // =======================
+  // ==== read use data in eeprom ====
+  LOG_INFO("Reading configuration from EEPROM...\n");
+  if (eeprom_read(&eeprom_08, EEPROM_HOLD_TIME_ADDR, (uint8_t *)&saved_hold_time, sizeof(saved_hold_time)) == I2C_STATE_OK)
+  {
+    if ((saved_hold_time >= MIN_HOLD_TIME) && (saved_hold_time <= MAX_HOLD_TIME))
+    {
+      current_hold_time = saved_hold_time;
+      LOG_INFO("Restored user setting from EEPROM: %lu ms\n", saved_hold_time);
+    }
+    else
+    {
+      current_hold_time = LID_HOLD_TIME;
+      saved_hold_time = LID_HOLD_TIME;
+      eeprom_write(&eeprom_08, EEPROM_HOLD_TIME_ADDR, (uint8_t *)&saved_hold_time, sizeof(saved_hold_time));
+      LOG_INFO("Invalid EEPROM data. Initialized with default: %d ms\n", LID_HOLD_TIME);
+    }
+  }
+  else
+  {
+    LOG_ERR("Please check eeprom state!\n");
+  }
+  // =================================
   /* USER CODE END 2 */
 
   /* Infinite loop */
@@ -141,28 +179,79 @@ int main(void)
     if (hcsr04_is_data_ready(&hcsr04_1))
     {
       dist = hcsr04_get_distance(&hcsr04_1);
+      uint32_t current_tick = HAL_GetTick();
       // LOG_DEBUG("Dist: %lu.%lu cm\n", dist / 10, dist % 10);
       switch (lid_state)
       {
         case LID_CLOSED:
+        {
+          // update `saved_hold_time` if nobody uses after [last_close_tick + COOL_DOWN_TIMEOUT] ms
+          if (need_cool_down_check && (current_tick - last_close_tick > COOL_DOWN_TIMEOUT))
+          {
+            need_cool_down_check = false;
+            if (current_hold_time > saved_hold_time)
+            {
+              // continuous used
+              saved_hold_time = current_hold_time;
+              eeprom_write(&eeprom_08, EEPROM_HOLD_TIME_ADDR, (uint8_t *)&saved_hold_time, sizeof(saved_hold_time));
+              normal_use_count = 0;
+              LOG_INFO("Habit recorded! Saving extended time %lu ms to EEPROM.\n", saved_hold_time);
+            }
+            else
+            {
+              // normal used
+              normal_use_count++;
+              if (normal_use_count >= DECREASE_THRESHOLD && current_hold_time > LID_HOLD_TIME)
+              {
+                current_hold_time -= 100;
+
+                if (current_hold_time < LID_HOLD_TIME)
+                  current_hold_time = LID_HOLD_TIME;
+
+                saved_hold_time = current_hold_time;
+                eeprom_write(&eeprom_08, EEPROM_HOLD_TIME_ADDR, (uint8_t *)&saved_hold_time, sizeof(saved_hold_time));
+                normal_use_count = 0;
+                LOG_INFO("System cooled down. Gradually reducing hold time to: %lu ms\n", saved_hold_time);
+              }
+            }
+          }
+          // open lib
           if (dist < 100 && dist > 20)
           {
+            // check if need to update `current_hold_time`
+            if (last_close_tick != 0)
+            {
+              // continuous used, update `current_hold_time` if elapsed time < 5s
+              if ((current_tick - last_close_tick) < 5000)
+              {
+                current_hold_time += TIME_INCREMENT;
+
+                if (current_hold_time > MAX_HOLD_TIME)
+                {
+                  current_hold_time = MAX_HOLD_TIME;
+                }
+                
+                normal_use_count = 0;
+                LOG_INFO("Continuous use! Dynamic hold time: %lu ms\n", current_hold_time);
+              }
+            }
             sg90_set_angle(&sg90, (uint16_t)LID_OPEN_ANGLE);
-            lid_timer_tick = HAL_GetTick();      // record tick of opening lib
+            lid_open_tick = HAL_GetTick();      // record tick of opening lib
             lid_state = LID_OPENING;
             HAL_GPIO_WritePin(LED1_GPIO_Port, LED1_Pin, GPIO_PIN_SET);
             LOG_INFO("Object detected! Opening lid...\n");
           }
           break;
-
+        }
         case LID_OPENING:
+        {
           static uint32_t _detection_count = 0;
           if (dist < 100 && dist > 20)
           {
             _detection_count++;
-            if (_detection_count >= 5)
+            if (_detection_count >= 8)
             {
-              lid_timer_tick = HAL_GetTick();  // reset counter
+              lid_open_tick = HAL_GetTick();  // reset counter
               LOG_INFO("Keeping lid open...\n");
             }
           }
@@ -171,27 +260,32 @@ int main(void)
             _detection_count = 0;
           }
           
-          if ((HAL_GetTick() - lid_timer_tick >= LID_HOLD_TIME) && (_detection_count == 0))
+          if ((HAL_GetTick() - lid_open_tick >= current_hold_time) && (_detection_count == 0))
           {
             sg90_set_angle(&sg90, (uint16_t)LID_CLOSE_ANGLE);
-            lid_timer_tick = HAL_GetTick();
+            lid_open_tick = HAL_GetTick();
             lid_state = LID_CLOSING;
             HAL_GPIO_WritePin(LED1_GPIO_Port, LED1_Pin, GPIO_PIN_RESET);
             LOG_INFO("Time up! Closing lid...\n");
           }
           break;
-
+        }
         case LID_CLOSING:
+        {
+          if (lid_state == LID_CLOSING)
+          {
+            if (HAL_GetTick() - lid_open_tick >= LID_CLOSING_TIME)
+            {
+              lid_state = LID_CLOSED;
+              last_close_tick = HAL_GetTick();
+              need_cool_down_check = true;     // 蓋子關了，開啟冷卻倒數計時
+              LOG_INFO("Lid fully closed. Ready for next trigger.\n");
+            }
+          }
           break;
-      }
-    }
-
-    if (lid_state == LID_CLOSING)
-    {
-      if (HAL_GetTick() - lid_timer_tick >= LID_CLOSING_TIME)
-      {
-        lid_state = LID_CLOSED;
-        LOG_INFO("Lid fully closed. Ready for next trigger.\n");
+        }
+        default:
+          break;
       }
     }
   }
